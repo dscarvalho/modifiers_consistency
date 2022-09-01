44b40c9eada8d7aaef98bedea4d3e7741eeb89c2
Danilo S. Carvalho <danilo.carvalho@manchester.ac.uk>
2022-09-01 12:30:51 +0100

    - Added adjective type weight experiment. - Code refactoring.

--- experiment.py
+++ experiment.py
@@ -9,10 +9,10 @@ from dynaconf import settings
 from joblib import Parallel, delayed, cpu_count
 from tqdm import tqdm
 from encoding import PhraseEncoder, phrase_from_index, ENCODERS, VECOPS
-from data_access.adjectives_nouns import Nouns, IDX_ADJ_TYPES
+from data_access.adjectives_nouns import Nouns, Adjectives, IDX_ADJ_TYPES
 
 
-class SetDistanceExperiment:
+class Experiment:
     def __init__(self,
                  filepath: str = settings["phrase_file_path"],
                  encoder: str = settings["encoder"],
@@ -21,33 +21,36 @@ class SetDistanceExperiment:
                  strict: bool = settings["strict"]):
         with open(filepath) as anfile:
             self.phrases: List[List[List[int]]] = json.load(anfile)
-            self.group()
-            self.encoder: str = encoder
-            self.vecop: str = vecop
-            self.sent_enc: bool = sent_enc
-            self.strict: bool = strict
+
+        self.group()
+        self.encoder: str = encoder
+        self.vecop: str = vecop
+        self.sent_enc: bool = sent_enc
+        self.strict: bool = strict
+        self._enc = PhraseEncoder(self.encoder, self.sent_enc)
 
     def group(self):
         self.phrases.sort(key=lambda x: [(e[1], e[0]) for e in reversed(x)])
 
-    def run(self, noun: str = None) -> Tuple[Dict[Tuple[int], float], Dict[int, float]]:
+    def run(self, noun: str = None):
+        raise NotImplementedError
+
+
+class SetDistanceExperiment(Experiment):
+
+    def run(self, noun: str = None) -> Dict[Tuple[int], float]:
         stats = dict()
-        stats_weight = {IDX_ADJ_TYPES[idx]: 0.0 for idx in IDX_ADJ_TYPES}
-        enc = PhraseEncoder(self.encoder, self.sent_enc)
         nouns = Nouns()
         phrases = [p for p in self.phrases if (len(p) > 1 and (noun is None or list(nouns[noun]) in p))]
         i = 0
         for idx_phrase in tqdm(phrases, desc=f"Progress ({noun})"):
             # print("Phrase:", phrase_from_index(idx_phrase))
             phrase = " ".join(phrase_from_index(idx_phrase))
-            vec = enc.encode(phrase, self.vecop)
+            vec = self._enc.encode(phrase, self.vecop)
 
-            dist_phrase_word = [1 - torch.cosine_similarity(vec, enc.encode(" ".join(phrase_from_index([p])), self.vecop), dim=0).item()
+            dist_phrase_word = [1 - torch.cosine_similarity(vec, self._enc.encode(" ".join(phrase_from_index([p])), self.vecop), dim=0).item()
                                 for p in idx_phrase]
 
-            for idx in IDX_ADJ_TYPES:
-                stats_weight[IDX_ADJ_TYPES[idx]] += sum([dist_phrase_word[i] for i in range(len(dist_phrase_word))
-                                                         if idx_phrase[i][1] == idx]) / sum(dist_phrase_word)
             dist_comb_compl = list()
             for k in range(1, len(idx_phrase) + 1):
                 combs = combinations(idx_phrase, k)
@@ -59,8 +62,8 @@ class SetDistanceExperiment:
                         dupl.add(tuple(phrase_from_index(compl)))
                         if (compl):
                             # print("Complement:", phrase_from_index(compl))
-                            dist_compl = 1 - torch.cosine_similarity(enc.encode(" ".join(phrase_from_index(comb)), self.vecop),
-                                                                     enc.encode(" ".join(phrase_from_index(compl)), self.vecop),
+                            dist_compl = 1 - torch.cosine_similarity(self._enc.encode(" ".join(phrase_from_index(comb)), self.vecop),
+                                                                     self._enc.encode(" ".join(phrase_from_index(compl)), self.vecop),
                                                                      dim=0).item()
                             dist_comb_compl.append(dist_compl)
 
@@ -79,41 +82,88 @@ class SetDistanceExperiment:
                 print({tuple([IDX_ADJ_TYPES[t] for t in k]): np.mean(stats[k]) for k in stats})
             i += 1
 
-        return {tuple([IDX_ADJ_TYPES[t] for t in k]): np.mean(stats[k]) for k in stats}, stats_weight
+        return {tuple([IDX_ADJ_TYPES[t] for t in k]): np.mean(stats[k]) for k in stats}
 
 
-def exp_run(exp: SetDistanceExperiment):
+class AdjectiveTypeWeightExperiment(Experiment):
+    def run(self, noun: str = None) -> Dict[int, float]:
+        stats_weight = {IDX_ADJ_TYPES[idx]: list() for idx in IDX_ADJ_TYPES}
+        stats_weight_aggr = {IDX_ADJ_TYPES[idx]: 0.0 for idx in IDX_ADJ_TYPES}
+        nouns = Nouns()
+        phrases = [p for p in self.phrases if (len(p) > 1 and (noun is None or list(nouns[noun]) in p))]
+        for idx_phrase in tqdm(phrases, desc=f"Progress ({noun})"):
+            phrase = " ".join(phrase_from_index(idx_phrase))
+            vec = self._enc.encode(phrase, self.vecop)
+
+            dist_phrase_word = [1 - torch.cosine_similarity(vec, self._enc.encode(" ".join(phrase_from_index([p])), self.vecop), dim=0).item()
+                                for p in idx_phrase]
+
+            for idx in IDX_ADJ_TYPES:
+                stats_weight[IDX_ADJ_TYPES[idx]].append(sum([dist_phrase_word[i] for i in range(len(dist_phrase_word))
+                                                         if idx_phrase[i][1] == idx]) / sum(dist_phrase_word))
+
+        for idx in IDX_ADJ_TYPES:
+            stats_weight_aggr[IDX_ADJ_TYPES[idx]] = np.mean(stats_weight[IDX_ADJ_TYPES[idx]])
+
+        return stats_weight_aggr
+
+
+class AdjectiveWeightExperiment(Experiment):
+    def run(self, noun: str = None) -> Dict[str, float]:
+        adjectives = Adjectives()
+        nouns = Nouns()
+        stats_weight = {adj[0]: list() for adj in adjectives}
+        stats_weight_aggr = {adj[0]: 0.0 for adj in adjectives}
+        phrases = [p for p in self.phrases if (len(p) > 1 and (noun is None or list(nouns[noun]) in p))]
+        for idx_phrase in tqdm(phrases, desc=f"Progress ({noun})"):
+            phrase = " ".join(phrase_from_index(idx_phrase))
+            vec = self._enc.encode(phrase, self.vecop)
+
+            dist_phrase_word = [1 - torch.cosine_similarity(vec, self._enc.encode(" ".join(phrase_from_index([p])), self.vecop), dim=0).item()
+                                for p in idx_phrase]
+
+            for adj in stats_weight:
+                stats_weight[adj].append(sum([dist_phrase_word[i] for i in range(len(dist_phrase_word))
+                                              if idx_phrase[i][0] == adjectives[adj][0]]) / sum(dist_phrase_word))
+
+        for adj in stats_weight:
+            stats_weight_aggr[adj] = np.mean(stats_weight[adj])
+
+        return stats_weight_aggr
+
+
+EXPERIMENTS = {
+    "setdistance": SetDistanceExperiment,
+    "adjtypeweight": AdjectiveTypeWeightExperiment,
+    "adjweight": AdjectiveWeightExperiment
+}
+
+
+def exp_run(exp: Experiment):
     results = Parallel(n_jobs=cpu_count(True))(delayed(exp.run)(noun) for noun, idxs in Nouns())
-    totals = dict()
-    totals_weights = {IDX_ADJ_TYPES[idx]: 0.0 for idx in IDX_ADJ_TYPES}
+    aggr = dict()
+    avgs = dict()
 
-    for res, weights in results:
+    for res in results:
         for key in res:
-            if (key not in totals):
-                totals[key] = 0
-            totals[key] += res[key]
+            if (key not in aggr):
+                aggr[key] = list()
+            aggr[key].append(res[key])
 
-        for key in weights:
-            totals_weights[key] += weights[key]
+    for key in aggr:
+        avgs[key] = np.mean(aggr[key])
 
-    for key in totals:
-        totals[key] /= len(results)
-
-    for key in totals_weights:
-        totals_weights[key] /= len(exp.phrases)
-
+    exp_name = type(exp).__name__
     sent_config = "-sent" if exp.sent_enc else ""
     vecop = "-" + exp.vecop if not exp.sent_enc else ""
     strict = "-strict" if exp.strict else ""
+    results_path = f"{settings['results_path']}/{exp_name}"
 
-    if (not os.path.exists(settings["results_path"])):
-        os.makedirs(settings["results_path"])
+    if (not os.path.exists(results_path)):
+        os.makedirs(results_path)
 
-    with open(f"{settings['results_path']}/results_{exp.encoder}{sent_config}{vecop}{strict}.json", "w") as outfile:
-        json.dump({str(key): totals[key] for key in totals}, outfile, indent=2)
-
-    with open(f"{settings['results_path']}/weights_{exp.encoder}{sent_config}{vecop}{strict}.json", "w") as outfile:
-        json.dump(totals_weights, outfile, indent=2)
+    with open(f"{results_path}/results_{exp.encoder}{sent_config}{vecop}{strict}.json", "w") as outfile:
+        json.dump({str(key): avgs[key] for key in avgs}, outfile, indent=2)
 
 
 def main(argv):
@@ -125,14 +175,14 @@ def main(argv):
                     for strict in (True, False):
                         exp = SetDistanceExperiment(encoder=enc, vecop=vecop, sent_enc=sent_enc, strict=strict)
                         exp_run(exp)
-    else:
-        exp = SetDistanceExperiment()
+    elif (len(argv) > 1 and argv[1] in EXPERIMENTS):
+        exp = EXPERIMENTS[argv[1]]()
         sent_config = "sent" if exp.sent_enc else ""
-        vecop = "-" + exp.vecop if not exp.sent_enc else ""
         strict = "strict" if exp.strict else ""
         print("Running config:", settings["encoder"], settings["vecop"], sent_config, strict)
         exp_run(exp)
-
+    else:
+        print("Experiment not recognized. Must be one of: " + " | ".join(EXPERIMENTS.keys()))
 
 
 if __name__ == "__main__":
